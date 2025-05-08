68a803435b0c2dec62cecca6f462f92a8885506a
César Vázquez <agua.ceavaz97@gmail.com>
2025-05-08 17:48:26 -0600

    Update clase1_tarea.js
    Clase1_TareaCésar.js

--- semana1/clase1_tarea.js
+++ semana1/clase1_tarea.js
@@ -1,24 +1,21 @@
-// Ejercicio: Crear una calculadora simple que realice operaciones básicas (suma, resta, multiplicación y división) entre dos números.
+// Ejercicio: Crear una calculadora simple que realice operaciones básicas (Suma, resta, multiplicación y divisións)
 
 const { ask } = require('../helpers/input');
 
-async function main() {
-  const num1 = Number(await ask('Ingresa el primer número: '));
-  const num2 = Number(await ask('Ingresa el segundo número: '));
+async function main() 
+{
+    const num1 = Number(await ask('Ingresa el primer número: '));
+    const num2 = Number(await ask('Ingresa el segundo número: '));
 
-  const suma = num1 + num2;
-  const resta = num1 - num2;
-  const multiplicación = num1 * num2;
-  const division = num1 / num2;
-  // TODO: Implementar la resta, multiplicación y división
+    const suma = num1 + num2;
+    const resta = num1 - num2;
+    const multi = num1 * num2;
+    const div = num1 / num2;
 
-  console.log('Suma:', suma);
-  console.log('resta:', resta);
-  console.log('Multiplicación:', multiplicación);
-  console.log('División:', division);
-
-  // TODO: Mostrar el resultado de la resta, multiplicación y división
-
+    console.log("La suma de: ", num1, "+", num2, "=", suma);
+    console.log("La resta de: ", num1, "-", num2, "=", resta);
+    console.log("La multiplicación de: ", num1, "*", num2, "=", multi);
+    console.log("La división de :", num1, "/", num2, "=", div);
 }
 
 main();
