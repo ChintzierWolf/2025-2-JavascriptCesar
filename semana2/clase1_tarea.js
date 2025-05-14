7cda724f654bd57e626c6e5280c8ec60abc8c780
César Vázquez <agua.ceavaz97@gmail.com>
2025-05-13 18:01:49 -0600

    Update clase1_tarea.js

--- semana2/clase1_tarea.js
+++ semana2/clase1_tarea.js
@@ -1,9 +1,14 @@
 // Práctica para casa:
 // Imprimir números pares del 1 al 100
+
 const { ask } = require('../helpers/input');
 
-async function main() {
-
+async function main() 
+{
+ for (let i = 2; i <= 100; i += 2 )
+ {
+    console.log (i);
+ }
 }
 
 main();
