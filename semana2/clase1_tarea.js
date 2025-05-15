9c1ab18cab9f970fb143f0c21429f3e1af348e16
anyruvalcaba <ruvalcaba.any@gmail.com>
2025-05-14 20:56:15 -0600

    Tarea 1 terminada

--- semana2/clase1_tarea.js
+++ semana2/clase1_tarea.js
@@ -4,6 +4,19 @@ const { ask } = require('../helpers/input');
 
 async function main() {
 
+function imprimirPares(limite) {
+    console.log("Los números pares del 1 al " + limite + " son:");
+    for (let i = 2; i <= limite; i = i+ 2) {
+        console.log(i);
+    }
+}
+
+function main() {
+    let limite = 100;
+    imprimirPares(limite);
+}
+
+main();
 }
 
 main();
